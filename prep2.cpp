ee178a42f6e18d375e1b28052b786bad06863b21
caleb1000 <caleb.steinmetz@yahoo.com>
2022-10-24 23:07:30 -0400

    added to example prep2

--- prep2.cpp
+++ prep2.cpp
@@ -5,7 +5,10 @@ class clearance {
     int confidential = 1;
 
     void print_clearance(){
-        std::cout<<"Confidential = "<<confidential<<" Secret = "<<secret<<" Top Secret = "<<top_secret<<std::endl;
+        std::cout<<"Base: Confidential = "<<confidential<<" Secret = "<<secret<<" Top Secret = "<<top_secret<<std::endl;
+    }
+    void inc_ts(){
+        top_secret++;
     }
 
   protected:
@@ -15,28 +18,49 @@ class clearance {
 };
 
 class c : public clearance{
+public:
     void inc(){
-        confidential++;
-        secret++;
-        //top_secret++; illegal
+        //all members keep original control level
+        confidential++;//public
+        secret++;//protected
+        //top_secret++; illegal, only base class can modify
     }
 };
 
 class s : protected clearance{
+public:
     void inc(){
         //all public and protected members are inherited as protected members, private stays private
-        confidential++;
-        secret++;
-        //top_secret++; illegal
+        confidential++;//protected
+        secret++;//protected
+        //top_secret++; illegal, only base class can modify
     }
 };
 
 class ts : private clearance{
+public:
     void inc(){
         //all data members are inherited as private
+        confidential++;//private
+        secret++;//private
+        //top_secret++; illegal, only base class can modify
+    }
+};
+
+class ts_sci : public ts{
+public:
+    void try_inc(){
+        //confidential++; illegal, data members of ts are all private and can only be modified by that class, even though confidential started as public in the original base class it changed to private in ts
+    }
+};
+
+class s_sap : public s{
+public:
+    void try_inc(){
+        //because s data members are protected, inherited classes can still modify the values
         confidential++;
         secret++;
-        //top_secret++; illegal
+        //top_secret++; illegal, only base class can modify
     }
 };
 
@@ -46,26 +70,49 @@ int main() {
   //Protected: Members can be accessed by base and derived classes
   //Private: Members can only be accessed by only base class
 
-
+  std::cout<<"Base class:"<<std::endl;
   clearance c1;
-  c1.confidential = 2;
+  c1.print_clearance();//print original values
+  c1.confidential = 2;//modify public value
+  c1.print_clearance();
+  c1.inc_ts();
+  c1.print_clearance();
   //c1.secret = 2; Illegal
   //c1.top_secret =2; Illegal
+  std::cout<<""<<std::endl;
 
-  c c2;
+  std::cout<<"Derived public class: C"<<std::endl;
+  c c2; //public inherited
+  c2.print_clearance();
   c2.confidential = 2;
+  c2.print_clearance();
+  c2.inc();
+  c2.print_clearance();
+  c2.inc_ts();
+  c2.print_clearance();
   //c2.secret = 2; Illegal
   //c2.top_secret =2; Illegal
+  std::cout<<""<<std::endl;
 
-  s c3;
+  std::cout<<"Derived protected class: S"<<std::endl;
+  s c3; //protected inherited
+  //c3.print_clearance(); Illegal
+  //c3.inc(); Illegal
+  //c3.print_clearance(); Illegal
   //c3.confidential = 2; Illegal
   //c3.secret = 2; Illegal
   //c3.top_secret =2; Illegal
+  std::cout<<""<<std::endl;
 
-  ts c4;
-  //c4.confidential = 2;
-  //c4.secret = 2;
-  //c4.top_secret = 2;
+  std::cout<<"Derived private class: TS"<<std::endl;
+  ts c4; //private inherited
+  //ts.print_clearance(); Illegal
+  //ts.inc(); Illegal
+  //ts.print_clearance(); Illegal
+  //c4.confidential = 2; Illegal
+  //c4.secret = 2; Illegal
+  //c4.top_secret = 2; Illegal
+  std::cout<<""<<std::endl;
 
   return 0;
 }
